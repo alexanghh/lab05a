aead839030b4a6cedee6b98fa757e7ec09e25fb3
Alex Ang HH <alexanghh@gmail.com>
2025-11-25 10:44:01 +0000

    Fixed error introduced.

--- Rational/src/Rational.java
+++ Rational/src/Rational.java
@@ -2,7 +2,7 @@
 public class Rational {
     long numerator,denominator;
 
-    class errorIllegal extends Exception {
+    class Illegal extends Exception {
         String reason;
         Illegal (String reason) {
             this.reason = reason;
